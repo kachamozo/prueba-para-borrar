77fca188ee250fbdff661163c3e01b7d6d08c973
kachamozo <kachamozo@yahoo.com>
2022-07-26 21:31:13 -0400

    primera prueba

---
+++ index.js
@@ -0,0 +1,14 @@
+const array = [1, 2, 3, 4, 5];
+
+const multi = array.map((e) => e * 2);
+console.log(multi);
+console.log('hola mundo');
+
+const persona = {
+	nombre: 'Roly',
+	apellido: 'Ochoa',
+	edad: 35,
+	estado: 'soltero',
+};
+
+console.log(persona.edad);
